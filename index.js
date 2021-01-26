6f187562eb2d3ac07ce2c1b0f1677f9ae8692f31
James Harrison <james@fasttortoise.co.uk>
2021-01-26 16:54:50 +0000

    🍠 Work on kicking the user with a message

--- index.js
+++ index.js
@@ -1,62 +1,60 @@
-//Require
+//Logger first
 const logger = require("log4js").getLogger("MinecraftCapes");
+logger.level = "debug";
+logger.info("Loading libraries...")
+
+//Check env
+const fs = require('fs')
+try {
+    if (!fs.existsSync('.env')) {
+        fs.copyFileSync('.env.example', '.env');
+    }
+} catch(err) {
+    logger.severe("Could not read .env file");
+    process.exit();
+}
+
+//Loads Libraries
 require('dotenv').config()
 const mc = require('minecraft-protocol');
-const mcData = require('minecraft-data')
 
-//Start Logs
-logger.level = "debug";
+/**
+ * Start the MC Server
+ */
 logger.info("Starting MinecraftCapes Auth...");
-
-//Minecraft Server
 const server = mc.createServer({
     host: process.env.SERVER_IP,
     port: process.env.SERVER_PORT,
     maxPlayers: 1,
     motd: process.env.MOTD,
     encryption: process.env.ENCRYPTION,
-    'online-mode': process.env.ONLINE_MODE
+    'online-mode': process.env.ONLINE_MODE,
 });
-logger.info("Started MinecraftCapes Auth on", server.socketServer);
+logger.info("Started MinecraftCapes Auth on", process.env.SERVER_IP + ":" + process.env.SERVER_PORT);
 
+/**
+ * Handle client connections
+ */
 server.on('login', function (client) {
+    const mcData = require('minecraft-data')(client.version)
     let loginPacket = mcData.loginPacket
-
-    client.write('login', {
-        entityId: client.id,
-        isHardcore: false,
-        gameMode: 0,
-        previousGameMode: 255,
-        worldNames: loginPacket.worldNames,
-        dimensionCodec: loginPacket.dimensionCodec,
-        dimension: loginPacket.dimension,
-        worldName: 'minecraft:overworld',
-        hashedSeed: [0, 0],
-        maxPlayers: server.maxPlayers,
-        viewDistance: 10,
-        reducedDebugInfo: false,
-        enableRespawnScreen: true,
-        isDebug: false,
-        isFlat: false
-    });
-    client.write('position', {
-        x: 0,
-        y: 1.62,
-        z: 0,
-        yaw: 0,
-        pitch: 0,
-        flags: 0x00
-    });
-    var msg = {
-        translate: 'chat.type.announcement',
-        "with": [
-            'Server',
-            'Hello, world!'
-        ]
-    };
-    client.write("chat", {
-        message: JSON.stringify(msg),
-        position: 0,
-        sender: '0'
-    });
+    client.end(
+        "§8§l§m===============================\n\n" +
+        `${getAuthCode(client.uuid)}` +
+        "\n\n§8§l§m===============================\n" +
+        "\n§bJoin our Minecraft Server" +
+        "\n§a\u25A0 §ePlay.CapeCraft.Net §a\u25A0"
+    );
 });
+
+function getAuthCode(uuid) {
+    let errorMessage = "§c§lSomething went wrong\nPlease reconnect to try again"
+    let blockedMessage = "§c§lThe specified account has been banned for violating our terms of service."
+
+    uuid = uuid.replaceAll("-", "");
+
+    let authCode = "123456"
+    let authMessage = `§fYour authorization code is\n§c§l\u00BB§f ${authCode} §c§l\u00AB`;
+
+    return authMessage;
+}
